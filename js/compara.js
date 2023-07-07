8aaa04a1f7202f95da2b6a9159ab84f623a00ca7
Igor Forner <igorfornerrodrigues2@gmail.com>
2023-07-07 12:56:26 -0300

    Adiciona pagina de registro

---
+++ js/compara.js
@@ -0,0 +1,12 @@
+let info = document.getElementsByClassName("info")
+let senhas = document.getElementsByClassName("senhacompara")
+
+function comparaSenhas(){
+    if (senhas[0].value != senhas[1].value){
+        info[0].style.display = "block"
+        info[1].style.display = "block"
+    }else{
+        info[0].style.display = "none"
+        info[1].style.display = "none"
+    }
+}
