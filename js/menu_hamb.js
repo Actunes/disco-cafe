bd8d6e51876dfc5e27c722399e2f3ab7ed5ba9f3
Igor Forner <igorfornerrodrigues2@gmail.com>
2023-07-12 00:05:15 -0300

    Melhora função que altera fonte ao clicar

--- js/menu_hamb.js
+++ js/menu_hamb.js
@@ -4,7 +4,11 @@ let menuAside = document.getElementById("menu_aside")
 menuHam.addEventListener("click", function () {
     if (menuAside.style.display === "none") {
         menuAside.style.display = "block"
+        menuHam.classList.remove("fa-bars")
+        menuHam.classList.add("fa-x")
     } else {
         menuAside.style.display = "none"
+        menuHam.classList.remove("fa-x")
+        menuHam.classList.add("fa-bars")
     }
 })
